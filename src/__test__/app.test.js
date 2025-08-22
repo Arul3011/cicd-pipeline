f9a6930df925bde5b311c22f1f4bd977cf5ba55a
unknown-1-me <badboy301104@gmail.com>
2025-08-22 15:56:31 +0000

    chanege dep and test

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -5,6 +5,5 @@ import React from 'react';
 jest.mock("../assets/data.txt?raw", () => "Mocked data content");
 test('renders App component without crashing', () => {
   render(<App />);
-  const heading = screen.getByText(/welcome/i); 
-  expect(heading).toBeInTheDocument();
+
 });
